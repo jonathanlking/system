e08d28d600beb426dffbc22314b48eed264a2c43
Jonathan King <jonathanlking@me.com>
2021-03-08 12:29:20 +0000

    Fixup fzf completion to ignore untracked files

--- nixos-config/home.nix
+++ nixos-config/home.nix
@@ -29,7 +29,7 @@ in {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    fileWidgetCommand = "fd --type f";
+    defaultCommand = "fd --type f";
   };
 
   programs.zsh = {
