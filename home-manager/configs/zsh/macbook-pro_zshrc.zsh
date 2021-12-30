4d06ad710fb6d4f74d07e87e0997fd9d18c39d24
Jonathan King <jonathanlking@me.com>
2021-12-30 17:30:08 +0000

    delete unused things from zshrc

--- home-manager/configs/zsh/macbook-pro_zshrc.zsh
+++ home-manager/configs/zsh/macbook-pro_zshrc.zsh
@@ -1,32 +1,3 @@
-typeset -U path
-path=(
-      $HOME/.nix-profile/bin
-      /run/current-system/sw/bin
-      $HOME/.local/cargo/bin
-      $HOME/.config/emacs/bin
-      $HOME/.npm-packages/bin
-      $HOME/.poetry/bin
-      $HOME/.local/flutter/bin
-      /Applications/Tailscale.app/Contents/MacOS
-      $HOME/.local/zig
-      $HOME/go/bin
-      $HOME/.ghcup/bin
-      /Applications/Julia-1.5.app/Contents/Resources/julia/bin
-      $HOME/.gem/ruby/2.6.0/bin/
-      /Users/michael/n/bin
-      /Library/TeX/Root/bin/x86_64-darwin/
-      /Users/michael/.npm-packages/bin
-      /usr/local/bin
-      /usr/local/sbin
-      /usr/bin
-      /bin
-      /sbin
-      $path
-    )
-
-export TERMINFO=$HOME/.config/terminfo
-export CLICOLOR=1
-
 home-upgrade () {
   nix flake update $HOME/system/home-manager
 }
