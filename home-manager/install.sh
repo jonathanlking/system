de1ab60f32d136c5367f3c6b46154d0ea7cfc862
Jonathan King <jonathanlking@me.com>
2021-12-30 17:16:53 +0000

    Fix the install script

--- home-manager/install.sh
+++ home-manager/install.sh
@@ -1,4 +1,29 @@
-# Where first arg is directory under machines, can be one of macos, fedora, nixos
-mkdir -p $HOME/.config
-ln -s $(pwd) $HOME/.config
-nix-shell -p nixUnstable --command "nix build --experimental-features 'nix-command flakes' '.#$1'"
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage() {
+  cat <<EOF
+Usage: $0 <MACHINE-NAME>
+
+Configure a machine according to its home-manager description.
+
+Arguments:
+
+<MACHINE-NAME>
+  The name of the machine, e.g. "macbook-pro".
+  These correspond to the attributes under homeConfigurations in the home-manager/flake.nix
+EOF
+  exit 1
+}
+
+main() {
+  [[ $# -gt 0 ]] || usage
+  local -r machine_name="$1"; shift
+
+  echo "Initialising $machine_name"
+  nix build --experimental-features "nix-command flakes" ".#$machine_name"
+  ./result/activate
+}
+
+main "$@"
