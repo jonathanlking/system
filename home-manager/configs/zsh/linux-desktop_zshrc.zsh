47b18751c5f04fee08c87afd5116603ef05dccef
Jonathan King <979725+jonathanlking@users.noreply.github.com>
2022-02-23 12:38:30 +0000

    ZSH improvements

--- home-manager/configs/zsh/linux-desktop_zshrc.zsh
+++ home-manager/configs/zsh/linux-desktop_zshrc.zsh
@@ -6,8 +6,11 @@ home-switch () {
   home-manager switch --flake "$HOME/system/home-manager#linux-desktop"
 }
 
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+
 # Use keychain to manage ssh keys
 eval $(keychain -q --agents ssh --eval)
 
 zle -N edit-command-line
-bindkey "^e" edit-command-line
+bindkey "^v" edit-command-line
