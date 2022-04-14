e10b2a0e5f3643b9ef83717a21bfe6ce7e006fc4
Jonathan King <jonathanlking@me.com>
2022-04-14 22:12:01 +0100

    Better time statistics

--- home-manager/configs/zsh/macbook-pro_zshrc.zsh
+++ home-manager/configs/zsh/macbook-pro_zshrc.zsh
@@ -12,3 +12,21 @@ home-switch () {
 
 # Use keychain to manage ssh keys
 eval $(keychain -q --agents ssh --eval)
+
+# Include more statistics with `time`
+if [[ `uname` == Darwin ]]; then
+  MAX_MEMORY_UNITS=KB
+else
+  MAX_MEMORY_UNITS=MB
+fi
+
+TIMEFMT=$(cat <<-END
+  %J   %U  user %S system %P cpu %*E total
+  avg shared (code):         %X KB
+  avg unshared (data/stack): %D KB
+  total (sum):               %K KB
+  max memory:                %M $MAX_MEMORY_UNITS
+  page faults from disk:     %F
+  other page faults:         %R
+END
+)
