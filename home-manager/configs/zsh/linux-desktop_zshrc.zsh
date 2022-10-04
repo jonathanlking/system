430c40066cdb1a34867a2431937c262db41bb63a
Jonathan King <jonathanlking@me.com>
2022-10-04 20:08:50 +0100

    Add notify command

--- home-manager/configs/zsh/linux-desktop_zshrc.zsh
+++ home-manager/configs/zsh/linux-desktop_zshrc.zsh
@@ -20,3 +20,17 @@ git-sed () {
   local replacement="$1"; shift
   git grep -z --files-with-matches -i "$pattern" | xargs -0 sed -i -e "s/$pattern/$replacement/g"
 }
+
+notify () {
+  local message=`cat /dev/stdin`
+  # TODO: Try to get working with env instead of source
+  # env $(cat ~/.secrets/pushover.env | xargs)
+  source ~/.secrets/pushover.env
+  curl -s \
+    --form-string "token=$PUSHOVER_TOKEN" \
+    --form-string "user=$PUSHOVER_USER" \
+    --form-string "message=$message" \
+    https://api.pushover.net/1/messages.json
+  unset PUSHOVER_TOKEN
+  unset PUSHOVER_USER
+}
